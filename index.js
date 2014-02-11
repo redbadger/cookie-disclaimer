503bca7b639b9ecaf55eeb5d07dad7ce4721af74
Alex Savin <karismafilms@gmail.com>
2014-02-11 16:37:37 +0000

    Dynamic creation of hmtl elements, content as argument

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
 var dom = require('dom');
 var store = require('store');
 
-template = require('./cookie');
-
-module.exports = function(target) {
+module.exports = function(target, content) {
   if(store('cookie-consent')) {
     return;
   }
   else {
-    d = dom(target);
-    d.prepend(template());
+    var d = dom(target);
 
-    button = dom('#close-disclaimer')[0];
+    // Create elements
+    var container = document.createElement('div');
+    container.id = 'cookie-disclaimer';
+
+    var customContent = document.createElement('div');
+    customContent.id = 'customContent';
+    customContent.innerHTML = content;
+    container.appendChild(customContent);
+
+    var actions = document.createElement('div');
+    actions.class = 'actions';
+    var button = document.createElement('button');
+    button.id = 'close-disclaimer';
+    button.innerText = 'Close';
+    actions.appendChild(button);
+
+    container.appendChild(actions);
+
+    d.prepend(container);
+
+    var button = dom('#close-disclaimer')[0];
     button.onclick = function(e) {
-      disclaimer = dom('#cookie-disclaimer')[0];
-      height = disclaimer.offsetHeight;
-      newValue = "-" + height + "px";
+      var disclaimer = dom('#cookie-disclaimer')[0];
+      var height = disclaimer.offsetHeight;
+      var newValue = "-" + height + "px";
       disclaimer.style.marginBottom = newValue;
       window.setTimeout(function(){
           disclaimer.remove();
